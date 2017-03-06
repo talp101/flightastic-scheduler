57d5f872632d0ea3135506937cf3438110bee8ff
Tal Peretz <13@1500.co.il>
2017-03-06 18:39:48 +0200

    ready for heroku

--- bin/server.js
+++ bin/server.js
@@ -2,6 +2,6 @@ const config = require('config');
 
 const app = require('../app')
 
-app.listen(config.get('port'), (request, response) => {
+app.listen((process.env.PORT || config.get('port')), (request, response) => {
     console.log(`Flightastic Scheduler started at port ${config.get('port')}`);
 })
