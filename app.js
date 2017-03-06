697f57341bc4dd4a385d8e94c02b2d447a01fb40
Ron Kazmirsky <ronikzkz@gmail.com>
2017-03-06 18:23:51 +0200

    working scheduael jobs

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const agenda = require('./lib/agenda');
+var Agendash = require('agendash');
+
 //  middlewares
 const errorHandler = require('./middlewares/error_handler');
 // routers
 const searchRouter = require('./routes/search_router');
 
 const app = new express();
+app.use(bodyParser.json());
+app.use('/agendash', Agendash(agenda));
 app.use('/search', searchRouter);
 app.use(errorHandler);
 
--- routes/search_router.js
+++ routes/search_router.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const agenda = require('../lib/agenda');
 
 const router = express.Router();
 
-router.post('/', (request, response, next) =>{    
+router.post('/', (request, response, next) =>{  
+    agenda.every('5 minutes', 'searchFlight', request.body); 
     response.json({success:true});
 })
 
